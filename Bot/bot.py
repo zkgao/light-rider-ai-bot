af1fa6115ac2e90cd7e344bb81b338e1f02c36fd
zkgao <42850514+zkgao@users.noreply.github.com>
2019-03-11 13:41:12 -0400

    Add files via upload

---
+++ Bot/bot.py
@@ -0,0 +1,28 @@
+import random
+import sys
+
+class Bot:
+
+    def __init__(self):
+        self.game = None
+
+    def setup(self, game):
+        self.game = game
+
+    def do_turn(self):
+        legal = self.game.field.legal_moves(self.game.my_botid, self.game.players)
+        if len(legal) == 0:
+            self.game.issue_order_pass()
+        else:
+            best=-100000
+            possible=[]
+            for l in legal:
+                herustic=self.game.field.h1(l,self.game.my_botid, self.game.players) *-1
+                if herustic > best:
+                    possible=[]
+                    possible.append(l)
+                elif herustic == best:
+                    possible.append(l)
+            (_,chosen)=(_, chosen) = random.choice(possible)
+            self.game.issue_order(chosen)
+    
