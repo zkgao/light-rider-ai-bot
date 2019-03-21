74008be9e88d14da389c42c4be3bd606f81a5a5d
kmkangutkar <komalkangutkar96@gmail.com>
2019-03-21 10:09:56 -0400

    Add minimax

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,6 +1,9 @@
 import random
 import sys
 
+TREE_DEPTH = 6
+LOST_VALUE = -10
+
 class Bot:
 
     def __init__(self):
@@ -25,18 +28,49 @@ class Bot:
         if len(legal) == 0:
             self.game.issue_order_pass()
         else:
-            best=-100000
-            possible=[]
-            for l in legal:
-                h1,h2,h3,h4,h5,h6,h7=self.get_herustic(l)
-                weight=[1,0,0,0,0,0,0]
-                herustic=h1*weight[0]+h2*weight[1]+h3*weight[2]+h4*weight[3]+h5*weight[4]+h6*weight[5]+h7*weight[6]
-                if herustic > best:
-                    possible=[]
-                    possible.append(l)
-                    best=herustic
-                elif herustic == best:
-                    possible.append(l)
-            (_,chosen)=(_, chosen) = random.choice(possible)
+            best_value = -100000
+            best_move = None
+            for move in legal:
+                self.game.field.field_forward(move, self.game.my_botid, self.game.players)
+                value = self.minimax((self.game.my_botid + 1)%2, 0, False)
+                self.game.field.field_reverse(move, self.game.my_botid, self.game.players)
+                if value > best_value:
+                    best_value = value
+                    best_move = move
+            # assert best_move != legal[0], "best move didn't get updated"
+            (_, chosen) = best_move
             self.game.issue_order(chosen)
+            
+    def minimax(self, curr_botid, depth, maximizing_player):
+        legal = self.game.field.legal_moves(curr_botid, self.game.players)
+        if len(legal) == 0:
+            #state is terminal
+            return LOST_VALUE
+        if depth == TREE_DEPTH:
+            # return self.game.field.h1(curr_botid, self.game.players)
+            h1,h2,h3,h4,h5,h6,h7=self.get_herustic(l)
+            weight=[1,0,0,0,0,0,0]
+            herustic=h1*weight[0]+h2*weight[1]+h3*weight[2]+h4*weight[3]+h5*weight[4]+h6*weight[5]+h7*weight[6]
+            return herustic
+        next_botid = (curr_botid + 1) % 2
+        if maximizing_player:
+            best_value = -10000
+            for move in legal:
+                self.game.field.field_forward(move, curr_botid, self.game.players)
+                value = self.minimax(next_botid, depth + 1, False)
+                self.game.field.field_reverse(move, curr_botid, self.game.players)
+                if value > best_value:
+                    best_value = value
+                    # best_move = move
+            return best_value
+        if not maximizing_player:
+            best_value = 10000
+            for move in legal:
+                self.game.field.field_forward(move, curr_botid, self.game.players)
+                value = self.minimax(next_botid, depth + 1, True)
+                self.game.field.field_reverse(move, curr_botid, self.game.players)
+                if value < best_value:
+                    best_value = value
+                    # best_move = move
+            return best_value          
     
