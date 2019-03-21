9446ad6e6e5c1e72bd9c0bb504de4f7215bf2883
zkgao <42850514+zkgao@users.noreply.github.com>
2019-03-21 09:46:18 -0400

    use weight list, organize all the herustic we have

--- Bot/bot.py
+++ Bot/bot.py
@@ -9,13 +9,17 @@ class Bot:
     def setup(self, game):
         self.game = game
     def get_herustic(self,l):
+            total_hit_dis=self.game.field.h1(l,self.game.my_botid, self.game.players)
+            min_hit_dis=self.game.field.h3(l,self.game.my_botid, self.game.players)
+            ki=self.game.field.ki(l,self.game.my_botid, self.game.players)
+            enemy_ki=self.game.field.ki(l,self.game.my_botid ^ 1, self.game.players)
             reachable,hlist=self.game.field.h2(l,self.game.my_botid, self.game.players)
             enemy_reachable,hlist2=self.game.field.h2(l,self.game.my_botid ^ 1, self.game.players)
             territory=0
             for i in range(400):
                 if hlist[i] < hlist2[i]:
                     territory+=1
-            return reachable,enemy_reachable,territory
+            return reachable,400-enemy_reachable,territory,total_hit_dis,min_hit_dis,ki,4-enemy_ki
     def do_turn(self):
         legal = self.game.field.legal_moves(self.game.my_botid, self.game.players)
         if len(legal) == 0:
@@ -24,8 +28,10 @@ class Bot:
             best=-100000
             possible=[]
             for l in legal:
-                herustic,herustic2,herustic3=self.get_herustic(l)
-                if -1*herustic2 > best:
+                h1,h2,h3,h4,h5,h6,h7=self.get_herustic(l)
+                weight=[1,0,0,0,0,0,0]
+                herustic=h1*weight[0]+h2*weight[1]+h3*weight[2]+h4*weight[3]+h5*weight[4]+h6*weight[5]+h7*weight[6]
+                if herustic > best:
                     possible=[]
                     possible.append(l)
                     best=herustic
